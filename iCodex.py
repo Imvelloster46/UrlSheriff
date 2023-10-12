b772118eb7e812558c6cc7c84a465ecdd8d38304
David Felipe <davifelipe17@gmail.com>
2023-10-12 18:21:57 -0300

    Ferramenta de Varredura de Sites com Interface Gráfica em Python Esta aplicação Python oferece uma interface gráfica simples para varredura de sites, permitindo aos usuários analisar páginas web em busca de informações específicas. Utilizando as bibliotecas Tkinter para a interface gráfica, requests para fazer requisições HTTP e BeautifulSoup para análise HTML, a ferramenta realiza as seguintes funcionalidades:
    Funcionalidades Principais:
    Varredura de Links:
    
    Procura por links que podem ser vulneráveis a ataques de força bruta, identificando potenciais áreas administrativas.
    Identificação de Formulários:
    
    Verifica se a página possui um formulário de login.
    Detecção de Imagens:
    
    Exibe a quantidade total de imagens na página e os URLs correspondentes.
    Links Externos:
    
    Identifica e exibe links externos presentes na página.
    Utilização:
    Insira a URL do site a ser verificado na entrada fornecida.
    Clique no botão "Executar" para iniciar a varredura.
    Os resultados serão exibidos na área de texto, destacando áreas administrativas, formulários, imagens e links externos.
    Outras Funcionalidades:
    Limpar Resultados:
    O botão "Limpar Resultados" permite apagar a área de texto para uma nova análise.
    Execução do Programa:
    Instale as dependências necessárias utilizando o comando pip install requests beautifulsoup4.
    Execute o script Pyth

---
+++ iCodex.py
@@ -0,0 +1,117 @@
+import socket
+from tkinter import *
+from tkinter import scrolledtext
+from urllib.parse import urljoin, urlparse
+
+import requests
+from bs4 import BeautifulSoup
+
+
+# Função para executar a varredura
+def executar_varredura():
+    # Limpa a área de resultados
+    txt.delete('1.0', END)
+
+    # Obtém a URL inserida pelo usuário
+    url = entry.get()
+
+    try:
+        # Faz uma requisição GET para a URL
+        resposta_http = requests.get(url)
+        resposta_http.raise_for_status()  # Lança uma exceção para erros HTTP
+        texto_html = resposta_http.text
+
+        # Parceia o HTML da página usando o BeautifulSoup
+        soup = BeautifulSoup(texto_html, 'html.parser')
+
+        # Procura por links que poderiam ser vulneráveis a ataques de força bruta
+        for link in soup.find_all('a'):
+            # Verifica se o link parece ser uma área restrita
+            if 'admin' in link.get('href'):
+                txt.insert(INSERT, f'Potencial área administrativa encontrada: {link.get("href")}\n')
+
+        # Verifica se a página possui um formulário de login
+        formulario = soup.find('form')
+        if formulario:
+            txt.insert(INSERT, 'Formulário de login encontrado.\n')
+
+        # Exibe a quantidade total de imagens e os URLs das imagens
+        imagens = soup.find_all('img')
+        txt.insert(INSERT, f'\nTotal de imagens encontradas: {len(imagens)}\n')
+        for img in imagens:
+            txt.insert(INSERT, f'URL da imagem: {urljoin(url, img["src"])}\n')
+
+        # Exibe links externos
+        links_externos = set()
+        for link in soup.find_all('a', href=True):
+            link_absoluto = urljoin(url, link['href'])
+            if urlparse(link_absoluto).netloc != urlparse(url).netloc:
+                links_externos.add(link_absoluto)
+        if links_externos:
+            txt.insert(INSERT, '\nLinks externos encontrados:\n')
+            for link in links_externos:
+                txt.insert(INSERT, f'{link}\n')
+
+    except requests.exceptions.RequestException as e:
+        txt.insert(INSERT, f'Erro ao acessar a URL: {e}\n')
+
+
+def verificar_portas_abertas(url):
+    # Obtem o nome do host a partir da URL
+    host = urlparse(url).hostname
+
+    # Lista de portas para verificar
+    portas_alvo = [80, 443, 8080]
+
+    # Tenta se conectar a cada porta
+    for porta in portas_alvo:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(1)
+
+            resultado = s.connect_ex((host, porta))
+
+            if resultado == 0:
+                txt.insert(INSERT, f'A porta {porta} está aberta\n')
+            else:
+                txt.insert(INSERT, f'A porta {porta} está fechada\n')
+
+            s.close()
+
+        except Exception as ex:
+            txt.insert(INSERT, f'Erro ao verificar a porta {porta}: {ex}\n')
+
+# Função para limpar os resultados na área de texto
+def limpar_resultados():
+    txt.delete('1.0', END)
+
+
+# Cria a janela principal
+janela = Tk()
+
+# Configura a janela
+janela.title('Ferramenta de Varredura de Sites')
+janela.geometry('800x600')
+
+# Cria um rótulo para a entrada da URL
+lbl1 = Label(janela, text='Insira a URL do site a ser verificado:')
+lbl1.grid(row=0, column=0, padx=10, pady=10)
+
+# Cria uma entrada para a URL do site
+entry = Entry(janela, width=40)
+entry.grid(row=0, column=1, padx=10, pady=10)
+
+# Cria um botão para iniciar a varredura
+btn_executar = Button(janela, text='Executar', command=executar_varredura)
+btn_executar.grid(row=0, column=2, padx=10, pady=10)
+
+# Cria um botão para limpar os resultados
+btn_limpar = Button(janela, text='Limpar Resultados', command=limpar_resultados)
+btn_limpar.grid(row=0, column=3, padx=10, pady=10)
+
+# Cria uma área de texto para exibir os resultados
+txt = scrolledtext.ScrolledText(janela, wrap=WORD)
+txt.grid(row=1, column=0, columnspan=4, padx=10, pady=10)
+
+# Loop principal da janela
+janela.mainloop()
